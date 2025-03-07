39c40d8b69efe5dac894ea1697abc79417bb8772
G-Braz <guilhermelordmud@gmail.com>
2025-03-07 03:01:48 -0300

    footer e navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,9 +4,10 @@ import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
+
 export const metadata: Metadata = {
-  title: "Desafio Next.JS 2024.2",
-  description: "Desafio Next.JS, Typescript, Tailinwd, Prisma, semestre 2024.2 para todos os membros da Code JR",
+  title: "GeckoUrban",
+  description: "Ecommerce de tenis",
 };
 
 export default function RootLayout({
